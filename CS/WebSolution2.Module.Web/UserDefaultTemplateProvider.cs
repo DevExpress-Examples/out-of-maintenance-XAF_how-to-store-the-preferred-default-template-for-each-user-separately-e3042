e0a9360acb2074e981459f8e6d36bd3c4433a6f0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSolution2.Module.Web/UserDefaultTemplateProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Web;
+
+namespace WebSolution2.Module.Web {
+    public class UserDefaultTemplateProvider {
+        public static string CurrentUserDefaultTemplate {
+            get {
+                HttpCookie userTemplateCookie = HttpContext.Current.Request.Cookies[SecuritySystem.CurrentUserId.ToString()];
+                if (userTemplateCookie != null && !String.IsNullOrEmpty(userTemplateCookie.Value)) {
+                    return userTemplateCookie.Value;
+                } else {
+                    return WebApplication.DefaultPage;
+                }
+            }
+            set {
+                HttpCookie userTemplateCookie = HttpContext.Current.Response.Cookies[SecuritySystem.CurrentUserId.ToString()];
+                userTemplateCookie.Value = value;
+                userTemplateCookie.Expires = DateTime.Now.AddMonths(1);
+            }
+        }
+    }
+}
