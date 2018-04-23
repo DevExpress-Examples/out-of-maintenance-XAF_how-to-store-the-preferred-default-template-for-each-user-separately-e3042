c6511698bd1cbbb665de3263928434a0027ade11
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSolution2.Module.Web/ChooseTemplateController.vb
@@ -0,0 +1,55 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Text
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.ExpressApp.Editors
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.DC
+Imports System.Web
+Imports DevExpress.ExpressApp.Web
+
+Namespace WebSolution2.Module.Web
+	Partial Public Class ChooseTemplateController
+		Inherits ViewController
+		Public Sub New()
+			InitializeComponent()
+			CreateActionItems()
+			RegisterActions(components)
+		End Sub
+		Private Sub CreateActionItems()
+			Dim defaultTemplateItem As New ChoiceActionItem("Horizontal navigation", "Default.aspx")
+			ChooseTemplateAction.Items.Add(defaultTemplateItem)
+			Dim defaultVerticalTemplateItem As New ChoiceActionItem("Vertical navigation", "DefaultVertical.aspx")
+			ChooseTemplateAction.Items.Add(defaultVerticalTemplateItem)
+		End Sub
+		Private Sub ChooseTemplateAction_Execute(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.Actions.SingleChoiceActionExecuteEventArgs) Handles ChooseTemplateAction.Execute
+			Dim prevUrl As String = HttpContext.Current.Request.RawUrl
+			Dim prevDefaultPage As String = UserDefaultTemplateProvider.CurrentUserDefaultTemplate
+			UserDefaultTemplateProvider.CurrentUserDefaultTemplate = e.SelectedChoiceActionItem.Data.ToString()
+			HttpContext.Current.Response.Redirect(prevUrl.Replace(prevDefaultPage, UserDefaultTemplateProvider.CurrentUserDefaultTemplate))
+		End Sub
+		Private Sub ChooseTemplateController_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Activated
+			AddHandler Frame.TemplateChanged, AddressOf Frame_TemplateChanged
+		End Sub
+
+		Private Sub Frame_TemplateChanged(ByVal sender As Object, ByVal e As EventArgs)
+			UpdateSelectedItem()
+		End Sub
+		Private Sub ChooseTemplateController_Deactivated(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Deactivated
+			RemoveHandler Frame.TemplateChanged, AddressOf Frame_TemplateChanged
+		End Sub
+		Private Sub UpdateSelectedItem()
+			For Each item As ChoiceActionItem In ChooseTemplateAction.Items
+				If UserDefaultTemplateProvider.CurrentUserDefaultTemplate.ToLower().Contains(item.Data.ToString().ToLower()) Then
+					ChooseTemplateAction.SelectedItem = item
+					Exit For
+				End If
+			Next item
+		End Sub
+	End Class
+End Namespace
---
+++ VB/WebSolution2.Web/ApplicationCode/WebApplication.vb
@@ -0,0 +1,89 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.ExpressApp.Web
+Imports DevExpress.ExpressApp
+Imports WebSolution2.Module.Web
+
+Namespace WebSolution2.Web
+	Partial Public Class WebSolution2AspNetApplication
+		Inherits WebApplication
+		Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+		Private module2 As DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule
+		Private module3 As WebSolution2.Module.WebSolution2Module
+		Private module4 As WebSolution2.Module.Web.WebSolution2AspNetModule
+		Private securityModule1 As DevExpress.ExpressApp.Security.SecurityModule
+		Private module6 As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+		Private sqlConnection1 As System.Data.SqlClient.SqlConnection
+		Private securitySimple2 As DevExpress.ExpressApp.Security.SecuritySimple
+		Private authenticationStandard1 As DevExpress.ExpressApp.Security.AuthenticationStandard
+		Private module5 As DevExpress.ExpressApp.Validation.ValidationModule
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Protected Overrides Function CreateFrameTemplateFactory() As IFrameTemplateFactory
+			Return New MyFrameTemplateFactory()
+		End Function
+		Private Sub WebSolution2AspNetApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+#If EASYTEST Then
+			e.Updater.Update()
+			e.Handled = True
+#Else
+			If System.Diagnostics.Debugger.IsAttached Then
+				e.Updater.Update()
+				e.Handled = True
+			Else
+				Throw New InvalidOperationException("The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application." & Constants.vbCrLf & "This error occurred  because the automatic database update was disabled when the application was started without debugging." & Constants.vbCrLf & "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " & "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " & "or manually create a database using the 'DBUpdater' tool." & Constants.vbCrLf & "Anyway, refer to the 'Update Application and Database Versions' help topic at http://www.devexpress.com/Help/?document=ExpressApp/CustomDocument2795.htm " & "for more detailed information. If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/")
+			End If
+#End If
+		End Sub
+
+		Private Sub InitializeComponent()
+			Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+			Me.module2 = New DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule()
+			Me.module3 = New WebSolution2.Module.WebSolution2Module()
+			Me.module4 = New WebSolution2.Module.Web.WebSolution2AspNetModule()
+			Me.module5 = New DevExpress.ExpressApp.Validation.ValidationModule()
+			Me.module6 = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+			Me.securityModule1 = New DevExpress.ExpressApp.Security.SecurityModule()
+			Me.sqlConnection1 = New System.Data.SqlClient.SqlConnection()
+			Me.securitySimple2 = New DevExpress.ExpressApp.Security.SecuritySimple()
+			Me.authenticationStandard1 = New DevExpress.ExpressApp.Security.AuthenticationStandard()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' module5
+			' 
+			Me.module5.AllowValidationDetailsAccess = True
+			' 
+			' sqlConnection1
+			' 
+			Me.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=WebSolution2;Integrated Security=SSPI;Pooling" & "=false"
+			Me.sqlConnection1.FireInfoMessageEventOnUserErrors = False
+			' 
+			' securitySimple2
+			' 
+			Me.securitySimple2.Authentication = Me.authenticationStandard1
+			Me.securitySimple2.UserType = GetType(DevExpress.Persistent.BaseImpl.SimpleUser)
+			' 
+			' authenticationStandard1
+			' 
+			Me.authenticationStandard1.LogonParametersType = GetType(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters)
+			' 
+			' WebSolution2AspNetApplication
+			' 
+			Me.ApplicationName = "WebSolution2"
+			Me.Connection = Me.sqlConnection1
+			Me.Modules.Add(Me.module1)
+			Me.Modules.Add(Me.module2)
+			Me.Modules.Add(Me.module6)
+			Me.Modules.Add(Me.module3)
+			Me.Modules.Add(Me.module4)
+			Me.Modules.Add(Me.module5)
+			Me.Modules.Add(Me.securityModule1)
+			Me.Security = Me.securitySimple2
+'			Me.DatabaseVersionMismatch += New System.EventHandler(Of DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs)(Me.WebSolution2AspNetApplication_DatabaseVersionMismatch);
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+	End Class
+End Namespace
